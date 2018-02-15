3deefa8a37f256df7b273f3c2a1ce2920861f63f
luketrevorrow <luke.trevorrow@gmail.com>
2018-02-15 13:54:03 +0000

    Initial Commit

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Running migrations"
+./manage.py migrate
+
+echo "Creating admin user"
+./manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@mycompany.com', 'adminpassword')"
+
+echo "Starting Django server"
+gunicorn djangounchained.wsgi:application
